26f38752ae3eedaf74afd0551e284d1a9622802e
naufal676 <101051596+naufal676@users.noreply.github.com>
2022-03-13 20:38:35 +0700

    Add files via upload

---
+++ week2/Sepeda.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class Sepeda {
+	//atribut
+	int gear = 5;
+	int jumlahRoda = 2;
+	String jenis = "Sepeda Gunung";
+	String merk = "Wimcycle";
+	
+	//method
+	void  ngerem () {
+		System.out.println("Sepeda direm");
+	}
+}
