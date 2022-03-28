8fe89c21d105527aabec2231028aca0541210e71
naufal676 <101051596+naufal676@users.noreply.github.com>
2022-03-28 19:55:02 +0700

    Add files via upload

---
+++ praktikum4/MotherboardAksi.java
@@ -0,0 +1,16 @@
+package praktikum4;
+
+import java.util.Scanner;
+
+public class MotherboardAksi {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		Motherboard.USB usb = new Motherboard.USB();
+		usb.usb2 = input.nextInt();
+		usb.usb3 = input.nextInt();
+		System.out.println("Total Ports = " +usb.getTotalPorts());
+	}
+
+}
